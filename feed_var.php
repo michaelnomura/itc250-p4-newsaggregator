4306c5666f211b1aca6057876a25fb4321d765b0
Michael Nomura <mnnomura@gmail.com>
2018-02-27 22:13:55 -0800

    feeds now sorted in order by date

--- feed_var.php
+++ feed_var.php
@@ -8,13 +8,15 @@
   $response = file_get_contents($request);
   array_push($xml, simplexml_load_string($response));
 
-  $request2 = "https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?ned=us&hl=en&gl=US";
+  $request2 = "https://gizmodo.com/rss";
   $response2 = file_get_contents($request2);
   array_push($xml, simplexml_load_string($response2));
 
-  $request3 = "https://news.google.com/news/rss/headlines/section/topic/TECHNOLOGY?ned=us&hl=en&gl=US";
+  $request3 = "https://www.wired.com/feed/rss";
   $response3 = file_get_contents($request3);
   array_push($xml, simplexml_load_string($response3));
+
+  $feeds = array();
 
   print '<h1>' . $xml->channel->title . '</h1>';
 
@@ -22,24 +24,53 @@
 
   foreach($xml as $feed)
   {
-      print '<h1>' . $feed->channel->title . '</h1>';
-      /*echo '<pre>';
-      var_dump($feed);
-      echo '</pre>';*/
       foreach($feed->channel->item as $story)
       {
-        echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
-        echo '<p>' . $story->description . '</p><br /><br />';
+        $articles = array ( 
+            'site'  => $feed->channel->title,
+            'title' => $story->title,
+            'desc'  => $story->description,
+            'link'  => $story->link,
+            'date'  => $story->pubDate,
+            );
+        
+        array_push($feeds, $articles);
       }
       
   }
 
+usort($feeds, function($a, $b) {
+    return strtotime($b['date']) - strtotime($a['date']);
+});
 
 
-/*  print '<h1>' . $xml->channel->title . '</h1>';
-  foreach($xml->channel->item as $story)
-  {
-    echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
-    echo '<p>' . $story->description . '</p><br /><br />';
-  }*/
-?>
+echo '<ul>';
+for ($x = 0; $x < sizeof($feeds); $x++) {
+    $site = $feeds[$x]['site'];
+    $title = str_replace(' & ', ' &amp; ', $feeds[$x]['title']);
+    $link = $feeds[$x]['link'];
+    $description = $feeds[$x]['desc'];
+    $date = date('l F d, Y', strtotime($feeds[$x]['date']));
+    
+    echo '<li>';
+    echo '<strong>'.$site.': <a href="'.$link.'" title="'.$title.'" target="_blank">'.$title.'</a><p>' . 
+    $description. '</p></strong> ('.$date.')';
+    echo '</li>';
+}
+echo '</ul>';
+
+/*$limit = 30;
+echo '<ul>';
+for ($x = 0; $x < $limit; $x++) {
+    $site = $feeds[$x]['site'];
+    $title = str_replace(' & ', ' &amp; ', $feeds[$x]['title']);
+    $link = $feeds[$x]['link'];
+    $description = $feeds[$x]['desc'];
+    $date = date('l F d, Y', strtotime($feeds[$x]['date']));
+    
+    echo '<li>';
+    echo '<strong>'.$site.': <a href="'.$link.'" title="'.$title.'" target="_blank">'.$title.'</a>' . 
+    $description. '</strong> ('.$date.')';
+    echo '</li>';
+}
+echo '</ul>';*/
